5571f2f3a94d726ff52e9ba71a8ee55ebc46e33d
William Hilton <wmhilton@gmail.com>
2019-01-09 14:41:39 -0500

    fix: don't expect errors in tests for deriveFormData

--- src/__tests__/deriveFormData.spec.ts
+++ src/__tests__/deriveFormData.spec.ts
@@ -16,22 +16,4 @@ describe('formtron/deriveFormData', () => {
       });
     }
   });
-
-  describe('bad data', () => {
-    for (const i of ['simple']) {
-      const schema = require(`./examples/${i}/schema.json`);
-      const data = require(`./examples/${i}/data.json`);
-      // Corrupt data
-      delete data.age;
-      test(`example ${i}`, () => {
-        let e = null;
-        try {
-          deriveFormData(schema, data, data._selection.slice(0, 1));
-        } catch (err) {
-          e = err;
-        }
-        expect(e).not.toBeNull();
-      });
-    }
-  });
 });
