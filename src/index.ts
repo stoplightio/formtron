2b9ecfad5989fb0a3990661ca6f7fe87a6197ad9
William Hilton <wmhilton@gmail.com>
2019-02-05 15:45:46 -0500

    chore: fix code

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-export { Formtron } from './Formtron';
-
-export { applyOps } from './applyOps';
-export { computeOps } from './computeOps';
-export { computeWarnings } from './computeWarnings';
-export { deriveFormData } from './deriveFormData';
-
-export * from './types';
