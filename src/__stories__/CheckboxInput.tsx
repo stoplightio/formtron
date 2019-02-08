1a6683298ad70ce9b3d950373a9b3838176b65eb
William Hilton <wmhilton@gmail.com>
2019-02-08 16:18:27 -0500

    feat: support 'strict' option [SL-1584]

--- src/__stories__/CheckboxInput.tsx
+++ src/__stories__/CheckboxInput.tsx
@@ -5,6 +5,7 @@ import { action } from '@storybook/addon-actions';
 import { withKnobs } from '@storybook/addon-knobs';
 import { boolean, text } from '@storybook/addon-knobs/react';
 
+import { Box } from '@stoplight/ui-kit/Box';
 import { CheckboxInput } from '../components/CheckboxInput';
 import { ThemeZone } from '../theme';
 
@@ -13,15 +14,17 @@ storiesOf('Inputs', module)
   .addDecorator(storyFn => <ThemeZone name="formtron">{storyFn()}</ThemeZone>)
   .add('CheckboxInput', () => {
     return (
-      <CheckboxInput
-        value={boolean('value', true)}
-        selection="/#"
-        schema={{
-          title: text('schema.title', 'Title'),
-          required: boolean('schema.required', false),
-        }}
-        onChange={action('onChange')}
-        fieldComponents={{}}
-      />
+      <Box width="300px">
+        <CheckboxInput
+          value={boolean('value', true)}
+          selection="/#"
+          schema={{
+            title: text('schema.title', 'Title'),
+            required: boolean('schema.required', false),
+          }}
+          onChange={action('onChange')}
+          fieldComponents={{}}
+        />
+      </Box>
     );
   });
